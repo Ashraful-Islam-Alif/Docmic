a7f94886a2e52447518a42881529559de6f53954
AshrafulIslamAlif <alif.ashrafulislam@gmail.com>
2022-09-13 16:39:24 +0600

    contact

--- src/Pages/HomePage/Contact/Contact.js
+++ src/Pages/HomePage/Contact/Contact.js
@@ -8,13 +8,13 @@ const Contact = () => {
                 <h2 className='text-3xl py-4 font-semibold'>What they say?</h2>
             </div>
             <div className='grid justify-items-center '>
-                <div className=' grid grid-cols-4 gap-4 p-4 bg-white contact-height box-shadow'>
-                    <div className='col-span-3'>
+                <div className=' grid grid-cols-4 lg:grid-cols-4 gap-4 p-4 bg-white contact-height box-shadow rounded-md'>
+                    <div className=' col-span-3'>
                         <h2>David Jeams</h2>
                         <h2 className='py-2'>Patient</h2>
                         <p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it is a long established fact that a reader will be distracted by the readable content </p>
                     </div>
-                    <div>
+                    <div className='flex items-center '>
                         <img src="https://i.ibb.co/WxSJcGF/Ikbal-1.png" alt="" srcset="" />
                     </div>
                 </div>
